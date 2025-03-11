775bebacab38f42672a3df80cc84882f1a8e38ca
Orealyz <reguememartin@gmail.com>
2025-03-11 10:18:20 +0100

    Add script

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Devops-Ynov
+# WIK-DPS-TP01
---
+++ api.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+cd api/
+
+echo "PING_LISTEN_PORT=4000" > .env
+
+echo "Init du projet"
+npm init -y
+
+echo "Installation de TypeScript"
+npm install typescript --save-dev
+
+echo "Compilation TypeScript"
+npx tsc || echo "Erreur lors de la compilation TypeScript"
+
+echo "Lancement de l'application"
+node build/index.js
+
